365e105b2c9f169a7031df9d7f229469fcbb17eb
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-09-03 07:32:05 +0100

    created contact form and added to contact page

--- src/components/ContactForm/contactForm.js
+++ src/components/ContactForm/contactForm.js
@@ -1,12 +1,32 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
+import TextField from '@mui/material/TextField';
 import './contactForm.css';
+
 
 const ContactForm = () => {
     return (
-        <Grid item xs={4} sm={4} md={6}>
-            <Paper>Contact Form</Paper>
+        <Grid item xs={4} sm={4} md={6} className="contactForm">
+            <h4>Send me a Message:</h4>
+            <TextField
+                className="textField"
+                id="outlined-basic"
+                label="Name"
+                variant="outlined"
+            />
+            <TextField
+                className="textField"
+                id="outlined-basic"
+                label="Email address"
+                variant="outlined"
+            />
+            <TextField
+                className="textField"
+                id="outlined-multiline-static"
+                label="Your message"
+                multiline
+                rows={4}
+            />
         </Grid>
     )
 }
