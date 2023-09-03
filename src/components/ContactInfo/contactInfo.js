0c5546b2da05f07e508febd9b33d59eb0dbaf804
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-09-03 08:55:37 +0100

    added correct contact details

--- src/components/ContactInfo/contactInfo.js
+++ src/components/ContactInfo/contactInfo.js
@@ -7,8 +7,8 @@ const ContactInfo = () => {
     return (
         <Grid item xs={4} sm={4} md={6} className="contactInfo">
             <h4>Contact Info:</h4>
-            <p>Phone: 07123456789</p>
-            <p>Email: <span><a href="mailto:naomimardon@email.com">naomimardon@email.com</a></span></p>
+            <p>Phone: 07909 624 384</p>
+            <p>Email: <span><a href="mailto:naomi.mardon@gmail.com">naomi.mardon@gmail.com</a></span></p>
             <p>GitHub: <span><Link to="https://github.com/naomimardon" target="_blank" end>naomimardon</Link></span></p>
             <p>LinkedIn: <span><Link to="https://www.linkedin.com/in/naomi-mardon-79099455/" target="_blank" end>Naomi Mardon</Link></span></p>
         </Grid>
